f5408d4e291cf6fef1eb18c8a96df75deefc4d18
Tim Hutchinson <t.hutchinson@utah.edu>
2016-02-05 00:38:54 +0700

    get max number of tweets

--- python/get_tweets.py
+++ python/get_tweets.py
@@ -16,7 +16,7 @@ ACCESS_SECRET = tokens[3].strip('\n')
 
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 lis = [695067860435992576] ## Latest starting tweet id
-for i in range(0,1):
+for i in range(0,16):
     user_timeline = twitter.get_user_timeline(screen_name='KimKardashian', count=200,
                                               include_retweets=False, max_id=lis[-1])
     for tweet in user_timeline:
@@ -38,4 +38,4 @@ for i in range(0,1):
                 lis.append(tweet['id'])
             except UnicodeEncodeError:
                 pass
-    #time.sleep(301) ## 5 minutes betwen api calls
+    time.sleep(301) ## 5 minutes betwen api calls
