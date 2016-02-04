6d4045c4604c5e7365624d3069232ac97e4f54cb
Tim Hutchinson <t.hutchinson@utah.edu>
2016-02-03 21:57:33 -0700

    added

---
+++ python/generate_tweet.py
@@ -0,0 +1,19 @@
+import pickle
+import random
+
+chain = pickle.load(open('../data/chain.p', 'rb'))
+
+def generate_tweet():
+    new_tweet = []
+    sword1 = 'BEGIN'
+    sword2 = 'NOW'
+
+    while True:
+        sword1, sword2 = sword2, random.choice(chain[(sword1, sword2)])
+        if sword2 == 'END':
+            break
+        new_tweet.append(sword2)
+
+    return ' '.join(new_tweet)
+
+print generate_tweet()
